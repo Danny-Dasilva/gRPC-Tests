ca7ff9eb74079fab2ba9105c7d7126308102795f
Danny Dasilva <yahchayildasilva@gmail.com>
2021-06-20 22:44:17 -0400

    Update main.js

--- js/client/main.js
+++ js/client/main.js
@@ -1,1 +1,26 @@
 
+var server = new grpc.Server();
+server.addService(grpcChat.ChatService.service, {
+    chat:chat
+});
+server.bind('0.0.0.0:50050', grpc.ServerCredentials.createInsecure());
+server.start();
+
+
+function chat(call) {
+call.on('data', function(chatRequest{
+    user=call.metadata.get('username');
+    msg=chatRequest.message;
+
+
+for (let [msgUser, userCall] of clients) {
+    if (msgUser != username) {
+        userCall.write(
+           {
+             fromName: username,
+             message : msg
+           });
+       }
+}if (clients.get(user) === undefined) {
+clients.set(user, call);
+}
